469d760d9185cc8e7df2801dd3f6e20341aa7a2f
Leanne Luce <step@Leannes-MacBook-Pro.local>
2017-11-10 20:44:20 -0800

    deploy to firebase

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,10 +8,10 @@ export default class Footer extends Component {
         <div className="container-fluid">
           <div className="row">
 
-            <div className="col-xs-6 col-sm-6">
+            <div className="col-xs-12 col-sm-6">
               <p>Website created by <a target="none" href="http://0xeye.com">0xeye</a></p>
             </div>
-            <div className="col-xs-6 col-sm-6">
+            <div className="col-xs-12 col-sm-6">
               <p className="text-right">Join us: <a href="https://meetup.com/fashion-technologists">Meetup</a> <br />
               <a className="text-right" href="mailto:info@fashion-technologists.com">info@fashion-technologists.com</a></p>
             </div>
